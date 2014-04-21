9d9d3f94a935a968284c41110b6e92940f893776
Your Name <you@example.com>
2014-04-21 14:55:23 +0800

    update
    更新记分
    更新外观
    没吃一个加速一档 最高10档

--- snake.js
+++ snake.js
@@ -14,16 +14,17 @@ var snakes = new Array();
 var foods = new Array();
 var int=setInterval(move,speed);
 var int2=setInterval(checkHit,speed);
+var score=0;
 canvas.style.left = width/2-250+"px";
 function init(){//初始化
 	ctx.clearRect(0,0,500,500);
-	ctx.fillStyle="yellow";
+	ctx.fillStyle="RGB(56,177,183)";
     for(var i = len-1;i>=0;i--){  
                snakes[i] = {x:200+(len-i)*10,y:200};  
                ctx.fillRect(snakes[i].x,snakes[i].y,10,10);  
            }
 	foods[0] = {x:parseInt(Math.round(Math.random()*49)*10),y:parseInt(Math.round(Math.random()*49))*10};
-	ctx.fillStyle="gray";  
+	ctx.fillStyle="rgb(32,96,130)";  
     ctx.fillRect(foods[0].x,foods[0].y,10,10);
 }
     init();  
@@ -32,7 +33,7 @@ function init(){//初始化
        //alert("_x:"+_x+";_y:"+_y);  
        ctx.clearRect(0,0,500,500);       
        //循环替代位置，从最后一个开始  
-	   ctx.fillStyle="yellow"; 
+	   ctx.fillStyle="RGB(56,177,183)"; 
 	   for(var i = len-1;i>=1;i-- ){
            //console.log(i);  
            snakes[i].x = snakes[i-1].x;  
@@ -42,20 +43,18 @@ function init(){//初始化
        snakes[0].x = _x;  
        snakes[0].y = _y;  
        ctx.fillRect(_x, _y, 10, 10);
-       ctx.save();
+      /* ctx.save();
 	   ctx.shadowColor = 'rgba(0, 0, 0, 0.2)';
 	   ctx.shadowOffsetX = 2;
 	   ctx.shadowOffsetY = 2;
 	   ctx.shadowBlur = 0;  
-       ctx.restore();
+       ctx.restore();*/
 	
-	   ctx.fillStyle="rgba(32,96,130,1)";
+	   ctx.fillStyle="rgb(32,96,130)";
        for(var i = 0;i<foods.length;i++){  
            ctx.fillRect(foods[i].x,foods[i].y,10,10);  
        }  
-       //print(); 
-   	   ctx.strokeStyle = "#CC0000"
-       ctx.strokeRect(0,0,500,500); 
+
    }  
 	$(document).keydown(function keydown(e){  
 		switch(e.keyCode){  
@@ -157,11 +156,13 @@ function init(){//初始化
 	draw(x,y);  
 	};  
 	function again(){
-		speed=100;
+		speed=200;
 		var d=document.getElementById('gameOver');
 		d.style.display = "none";
 		int=setInterval(move,speed);
 		int2=setInterval(checkHit,speed);
+		score=0;
+		document.getElementById("scoreNum").innerHTML=score;
 	}
 	function addSnake(){  
 	        snakes[len]= {x:snakes[len-1].x+10,y:snakes[len-1].y+10};  
@@ -181,7 +182,7 @@ function init(){//初始化
 	   if(foodN==0){
              break
 	   }
-   }
+    }
 	   foods[foods.length] = {x:fx,y:fy};
     } 
     function deleteFood(fIndex){
@@ -206,7 +207,10 @@ function init(){//初始化
             if((Math.abs(fx-x)<10)&&(Math.abs(fy-y)<10)){  
                 addSnake(); 
                 deleteFood(i); 
-				createFood();   
+				createFood();
+				score=score+5;
+				document.getElementById("scoreNum").innerHTML=score;
+				speedUp();
         }  
     }
 };
